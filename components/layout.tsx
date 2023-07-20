89e1576eae5c1f15e19faee67c73856802699c54
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-07-20 16:14:49 -0700

    Reformatted into typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,13 @@ import Link from 'next/link'
 const name = 'Grant Hale'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -65,5 +71,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,20 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../utils/posts';
-import Link from 'next/link';
-import Date from '../components/date';
+import { getSortedPostsData } from '../utils/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -35,8 +33,8 @@ export default function Home({ allPostsData }) {
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
-              <small className="lightText">
-                <br></br>
+              <br />
+              <small className={utilStyles.lightText}>
                 <Date dateString={date} />
               </small>
             </li>
@@ -45,4 +43,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
