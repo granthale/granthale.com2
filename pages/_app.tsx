e68b6f91b1d0de58ac84743816d864fe9ab89fe3
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-07-29 12:55:42 -0700

    Changed font face to roboto mono

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import "../dist/output.css";
 
-import { Alegreya } from "next/font/google";
-const alegreya = Alegreya({ subsets: ["latin"] });
+import { Roboto_Mono } from "next/font/google";
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 import { ThemeProvider } from "next-themes";
 
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={alegreya.className}>
+    <main className={robotoMono.className}>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
