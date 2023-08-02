bbd54606f0a72ac41b20512e8267f811cd24e301
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-02 14:36:02 -0700

    Removed tealish

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,10 @@ module.exports = {
       "primary": '#2563eb',
       "neon-green": '#22c55e',
       "waving-brown": '#F4C641',
-      "tealish": "#00FFC2",
       "white": "#FFFFFF",
       "background": "#FDF3F1",
       "gray": "#808080",
       "reddish":"#E55C41",
-      "black": "#000000",
       "blue": "#2563eb",
       "violet": "#7c3aed"
     }
