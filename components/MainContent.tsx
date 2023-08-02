535daf43b3898ada0a1fd9eedf384dc2dfd2e34e
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-02 13:50:42 -0700

    Rearranged index page

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,20 +1,10 @@
-import styles from "../styles/Layout.module.css";
-import SideBar from "./SideBar";
-
 interface Props {
   home?: boolean;
   children: React.ReactNode;
 }
 
 const MainContent = ({ home, children }: Props) => {
-  return (
-    <div className="flex flex-col">
-      <div className="hidden md:block sticky m-8 flex-none">
-        <SideBar home={home} />
-      </div>
-      <div className="m-8 overflow-y-auto">{children}</div>
-    </div>
-  );
+  return <div className="m-8 overflow-y-auto">{children}</div>;
 };
 
 export default MainContent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,59 @@
 import Layout from "../components/Layout";
 import Header from "../components/Header";
 import Arrow from "../components/Arrow";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header page="home" />
       <Layout home>
-        <div className="text-xl text-primary">
-          <p className="font-extrabold text-4xl">👋 Hi,I'm Grant!</p>
-          <br />
-          <p className="text-neon-green">
-            I believe that everyone has more room for{" "}
-            <span className="font-bold">curiosity and creativity</span>. I’m
-            living to broaden those qualities in myself and help others do the
-            same.
-          </p>
-          <br />
-          <ul className="list-disc text-black pl-5 text-xl">
-            <li key="podcast">
-              I’m asking questions on{" "}
-              <a
-                className="underline hover:text-neon-green"
-                href="https://podcasters.spotify.com/pod/show/surfacetensionpod"
-                target="_blank"
-              >
-                Surface Tension
-                <Arrow></Arrow>
+        <div className="flex">
+          <div className="text-xl text-primary">
+            <p className="font-extrabold text-4xl">👋 Hello!</p>
+            <br />
+            <p className="text-neon-green">
+              I'm Grant. I believe that everyone has more room for{" "}
+              <span className="font-bold">curiosity and creativity</span>. I’m
+              living to broaden those qualities in myself and help others do the
+              same.
+            </p>
+            <br />
+            <ul className="list-disc text-black pl-5 text-xl">
+              <li key="podcast">
+                I’m asking questions on{" "}
+                <a
+                  className="underline hover:text-neon-green"
+                  href="https://podcasters.spotify.com/pod/show/surfacetensionpod"
+                  target="_blank"
+                >
+                  Surface Tension
+                  <Arrow></Arrow>
                 </a>
-            </li>
-            <li key="travel">
-              I’m traveling the world, looking for perspective on society and
-              self
-            </li>
-            <li key="learning">
-              I’m learning about different fields to find problems worth
-              solving
-            </li>
-          </ul>
-          <br />
-          <p className="text-neon-green">
-            Click around to get to know me and don't be a stranger!
-          </p>
+              </li>
+              <li key="travel">
+                I’m traveling the world, looking for perspective on society and
+                self
+              </li>
+              <li key="learning">
+                I’m learning about different fields to find problems worth
+                solving
+              </li>
+            </ul>
+            <br />
+            <p className="text-neon-green">
+              Always happy to talk to someone new, reach out!
+            </p>
+          </div>
+          <div className="hidden md:block sticky m-8 flex-none">
+            <Image
+              src="/images/boat.png"
+              alt="boat"
+              width={200}
+              height={200}
+              priority={true}
+            />
+          </div>
         </div>
       </Layout>
     </>
