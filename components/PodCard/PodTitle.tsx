22407364240850c65701a1b5baf37a701696afea
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-03 13:03:44 -0700

    Updated pod title color

--- components/PodCard/PodTitle.tsx
+++ components/PodCard/PodTitle.tsx
@@ -8,7 +8,7 @@ const PodTitle = ({podcast} : Props) => {
   return (
     <a
       href={podcast.external_urls.spotify}
-      className="text-gray-900 font-bold text-xl mb-2 hover:text-neon-green"
+      className="text-neon-green font-bold text-xl mb-2"
       target="_blank"
     >
       {podcast.name}
