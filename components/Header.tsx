341af1900a3c9d01e77cda5f1c8b841b36d73dc6
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-05 18:32:03 -0700

    Updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,14 +14,14 @@ const Header = ({ page }: Props) => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content="Grant's bustling internet cafe." />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:image:type" content="image/png" />
+      <meta name="twitter:image:type" content="image/jpeg" />
       <meta
         name="twitter:image:secure_url"
-        content="https://www.granthale.com/images/dot.png"
+        content="https://www.granthale.com/images/man_earth.jpeg"
       />
       <meta
         name="twitter:image"
-        content="https://www.granthale.com/images/dot.png"
+        content="https://www.granthale.com/images/man_earth.jpeg"
       />
       <meta property="og:title" content={title} />
       <meta property="og:url" content="https://www.granthale.com" />
@@ -29,13 +29,13 @@ const Header = ({ page }: Props) => {
       <meta property="og:locale" content="en_US" />
       <meta
         property="og:image"
-        content="https://www.granthale.com/images/dot.png"
+        content="https://www.granthale.com/images/man_earth.jpeg"
       />
       <meta
         property="og:image:secure_url"
-        content="https://www.granthale.com/images/dot.png"
+        content="https://www.granthale.com/images/man_earth.jpeg"
       />
-      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:description" content="Grant's bustling internet cafe." />
