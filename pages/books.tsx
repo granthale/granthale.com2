7de65e5f02cd7cb5cee3f9a1e0c65a3ec78109ce
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-09 22:19:33 -0700

    Copied posts logic from next.js tutorial

---
+++ pages/books.tsx
@@ -0,0 +1,49 @@
+import utilStyles from "../styles/utils.module.css";
+import { GetStaticProps } from "next";
+import { getSortedPostsData } from "../utils/posts";
+import Link from "next/link";
+import Header from "../components/Header";
+import Layout from "../components/Layout";
+import BookCard from "../components/BookCard";
+
+const books = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) => {
+  return (
+    <>
+      <Header page="books" />
+      <Layout>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className="text-2xl font-bold">Books</h2>
+          <br />
+          <ul className="list-disc">
+            {allPostsData.map(({ id, title }) => (
+              <BookCard id={id} title={title}></BookCard>
+              //   <li className="text-xl hover:text-neon-green underline" key={id}>
+              //     <Link href={`/posts/${id}`}>{title}</Link>
+              //     <br />
+              //   </li>
+            ))}
+          </ul>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+export default books;
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,51 @@
+import Layout from "../../components/Layout";
+import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { getAllPostIds, getPostData } from "../../utils/posts";
+import Link from "next/link";
+import styles from "../../styles/Layout.module.css";
+
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className="font-bold text-2xl">{postData.title}</h1>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+      <br />
+      <div className={styles.backToHome}>
+        <Link href="/books">← Back to books</Link>
+      </div>
+    </Layout>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
