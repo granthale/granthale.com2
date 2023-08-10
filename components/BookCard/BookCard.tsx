419c262e1e31ee45d44d49396250dba9d4086bdf
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-09 22:26:47 -0700

    Updated spacing

--- components/BookCard/BookCard.tsx
+++ components/BookCard/BookCard.tsx
@@ -17,19 +17,18 @@ const BookCard = ({ id, title }: Props) => {
   return (
     <>
       <Link href={`/posts/${id}`}>
-        <div className="rounded-b border lg:rounded-b-none lg:rounded-r p-4 flex">
-          <Image
-            className=""
-            width={100}
-            height={100}
-            src="/images/surface-tension.png"
-            alt="surface tension logo"
-          />
-          <div className="flex-col ml-4">
+        <div className="rounded-b border lg:rounded-b-none lg:rounded-r flex justify-between p-4">
+          <div className="flex-col">
             <p className="text-2xl font-bold">{title}</p>
             <p>This book was pretty good</p>
           </div>
-          <br />
+          <Image
+            className=""
+            width={120}
+            height={120}
+            src="/images/surface-tension.png"
+            alt="surface tension logo"
+          />
         </div>
         <br />
       </Link>
