2ff9156b2cbc06955c743de403953e6fe30a7140
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-11 15:36:43 -0500

    Adjusted 404 page - still needs work

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,5 +4,13 @@ export default function Custom404() {
   <Head>
     <title>404</title>
   </Head>;
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col justify-center">
+        <p className="text-4xl text-neon-green">
+          404 - Page Not Found
+        </p>
+      </div>
+    </div>
+  );
 }
