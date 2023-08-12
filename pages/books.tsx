4bcf157f9bf4310a654db07b053f6a309d85ced9
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-11 19:50:22 -0500

    Updated formatting for 'back arrow'

--- pages/books.tsx
+++ pages/books.tsx
@@ -23,21 +23,18 @@ const books = ({
       <Header page="books" />
       <Layout>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <div className="text-violet">
-            <Link
-              className="text-violet underline hover:no-underline"
-              href="/influences"
-            >
+          <div className="flex justify-between text-violet">
+            <div>
+              <h1 className="text-3xl font-bold">Books</h1>
+              <br />
+              <p className="text-xl font-bold">Sorting coming soon...</p>
+              {/*  by: (rating, title, recency) */}
+            </div>
+            <Link className="underline hover:no-underline" href="/influences">
               ← Back to influences
             </Link>
-            <br />
-            <br />
-            <h1 className="text-3xl font-bold">Books</h1>
-            <br />
-            <p className="text-xl font-bold">Sorting coming soon...</p>
-            {/*  by: (rating, title, recency) */}
-            <br />
           </div>
+          <br />
           {allBooksData.map(({ id, title, author, summary, rating, href }) => (
             <BookCard
               key={id}
