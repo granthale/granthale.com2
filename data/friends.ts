aea062902014c98e2464f3fc5c75810b56c393ac
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-29 21:33:36 -0500

    Added some friends

--- data/friends.ts
+++ data/friends.ts
@@ -2,13 +2,29 @@ export const friends = [
     {
         name: "Neel Khare",
         link: "https://neelkhare.com/",
-    },,
+    },
     {
-      name: "Rohan Hirani",
-      link: "http://rohanhirani.me/",
+        name: "Rohan Hirani",
+        link: "http://rohanhirani.me/",
     },
     {
         name: "Luke Clancy",
         link: "https://lukeclancy.me/"
+    },
+    {
+        name: "Madhav Aggarwal",
+        link: "https://www.instagram.com/madhiaggy/"
+    },
+    {
+        name: "Aidan Murphy",
+        link: "https://twitter.com/adnmurphy"
+    },
+    {
+        name: "Sharad Doshi",
+        link: "https://www.instagram.com/sharadxo/"
+    },
+    {
+        name: "Jacob Swiney",
+        link: "https://twitter.com/cjacobswiney"
     },
 ]
