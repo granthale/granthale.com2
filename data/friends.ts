ce2de14504ca7bd29627aeb60931908032cd7288
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-30 15:45:01 -0500

    Updated friends

--- data/friends.ts
+++ data/friends.ts
@@ -11,20 +11,4 @@ export const friends = [
         name: "Luke Clancy",
         link: "https://lukeclancy.me/"
     },
-    {
-        name: "Madhav Aggarwal",
-        link: "https://www.instagram.com/madhiaggy/"
-    },
-    {
-        name: "Aidan Murphy",
-        link: "https://twitter.com/adnmurphy"
-    },
-    {
-        name: "Sharad Doshi",
-        link: "https://www.instagram.com/sharadxo/"
-    },
-    {
-        name: "Jacob Swiney",
-        link: "https://twitter.com/cjacobswiney"
-    },
 ]
