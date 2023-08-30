593590239efb279dd376905a4d767647d72670a2
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-08-29 21:28:43 -0500

    Updated friends

--- data/friends.ts
+++ data/friends.ts
@@ -1,18 +1,14 @@
 export const friends = [
     {
+        name: "Neel Khare",
+        link: "https://neelkhare.com/",
+    },,
+    {
       name: "Rohan Hirani",
       link: "http://rohanhirani.me/",
     },
     {
-        name: "Neel Khare",
-        link: "https://neelkhare.com/",
-    },
-    {
         name: "Luke Clancy",
         link: "https://lukeclancy.me/"
-    },
-    {
-        name: "Aidan Murphy",
-        link: "https://aidanmurphy.me/",
     },
 ]
