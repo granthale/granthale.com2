f8aad89ec44a79dab4b73f5c5723b3ba9a7bebef
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-09-10 15:09:30 -0500

    Added RSS

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,4 +1,4 @@
-import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaTwitter, FaGithub, FaLinkedin, FaRss } from "react-icons/fa";
 import { AiTwotoneMail } from "react-icons/ai";
 import React from "react";
 
@@ -29,28 +29,41 @@ const Socials = ({ menu }: Props) => {
       link: "mailto:g@granthale.com",
       icon: <AiTwotoneMail size={icon_size} />,
     },
+    {
+      name: "RSS",
+      link: "/rss.xml",
+      icon: <FaRss size={icon_size} />,
+    },
   ];
   const classy = `hover:text-neon-green hover:scale-110 transform transition duration-500 ease-in-out m-2`;
 
   return (
     <>
-    <div className="flex justify-end mx-2">
-      {socials.map((social) => (
-        <React.Fragment key={social.name}>
-          <br />
-          <button
-            key={social.name}
-            className={`hover:text-neon-green hover:scale-110 transform transition duration-500 ease-in-out m-2 ${menu ? "text-white" : ""}`}
-            onClick={() => window.open(social.link, "_blank")}
-          >
-            {social.icon}
-          </button>
-          <br />
-          <br />
-        </React.Fragment>
-      ))}
-    </div>
-    <br />
+      <div className="flex justify-end mx-2">
+        {socials.map((social) => (
+          <React.Fragment key={social.name}>
+            <br />
+            <button
+              key={social.name}
+              className={`hover:text-neon-green hover:scale-110 transform transition duration-500 ease-in-out m-2 ${
+                menu ? "text-white" : ""
+              }`}
+              onClick={() => {
+                if (social.name !== "RSS") {
+                  window.open(social.link, "_blank");
+                } else {
+                  window.location.href = social.link;
+                }
+              }}
+            >
+              {social.icon}
+            </button>
+            <br />
+            <br />
+          </React.Fragment>
+        ))}
+      </div>
+      <br />
     </>
   );
 };
