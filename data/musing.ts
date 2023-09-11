bfee6ca25c90dd7b7f612af86581521dc5a1aa64
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-09-10 19:10:23 -0500

    Changed link to RSS

--- data/musing.ts
+++ data/musing.ts
@@ -1,7 +1,7 @@
 export const musing = [
     {
       title: "Renaissance of RSS",
-      link: "/writing/rss",
+      link: "/writing/rss-renaissance",
       date: "2023-09-10",
       description: "Thoughts on coffee"
     },
