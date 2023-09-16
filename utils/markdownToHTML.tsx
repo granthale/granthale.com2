714ecb002c930153dfcda6f7e427501f2bc4589b
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-09-16 12:12:23 +0900

    Removed sanitize. No need if I trust my source

--- utils/markdownToHTML.tsx
+++ utils/markdownToHTML.tsx
@@ -1,14 +1,14 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
-import rehypeSanitize from "rehype-sanitize";
+// import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
 
 async function markdownToHTML(markdown) {
   const file = await unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeSanitize)
+    // .use(rehypeSanitize) // if markdown source not trusted
     .use(rehypeStringify)
     .process(markdown);
 
