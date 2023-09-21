f6445ee18474f58764d46d0e920c98ce10e11fd4
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-09-21 17:13:25 +0900

    Updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,6 @@
 import { Head } from "next/document";
 import { useRouter } from "next/router";
+import styles from "../styles/404.module.css";
 
 export default function Custom404() {
   <Head>
@@ -11,16 +12,15 @@ export default function Custom404() {
     router.back();
   };
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col justify-center">
-        <p className="text-4xl text-neon-green">404 - Page Not Found</p>
-        <button
-          onClick={goBack}
-          className="mt-4 bg-neon-green text-white py-2 px-4 rounded hover:bg-green-600"
-        >
-          Go Back
-        </button>
-      </div>
+    <div className={styles.container}>
+      <h1 className="text-4xl text-neon-green">404 - Page not found.</h1>
+      <br/>
+      <h1>
+        {" "}
+        <span className="ascii text-3xl">(╯°□°）╯︵ ┻━┻</span>
+      </h1>
+      <br/>
+      <button className="text-3xl hover:text-neon-green" onClick={goBack}>Go back</button>
     </div>
   );
 }
