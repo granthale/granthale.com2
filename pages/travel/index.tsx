67eaf041ecc3c84d4a930492d3d01bcd1085b2fc
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-11-10 10:50:39 +0800

    Added country notes

--- pages/travel/index.tsx
+++ pages/travel/index.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import Layout from "../components/Layout";
-import Header from "../components/Header";
+import Layout from "../../components/Layout";
+import Header from "../../components/Header";
+
 import {
   ComposableMap,
   Geographies,
   Geography,
   Graticule,
 } from "react-simple-maps";
+import Link from "next/link";
+import { GetStaticProps } from "next";
+import { getSortedData } from "../../utils/usePosts";
 
 const highlightCountries = [
   "USA",
@@ -25,13 +28,20 @@ const highlightCountries = [
   "PHL",
 ];
 
-const travel = () => {
+const travel = ({
+  allTravelData,
+}: {
+  allTravelData: {
+    title: string;
+    date: string;
+    id: string;
+  }[];
+}) => {
   return (
     <>
       <Header page="travel" />
       <Layout>
         <div className="text-neon-green font-extrabold text-4xl">travel!</div>
-        <br />
         <ComposableMap projectionConfig={{ scale: 180 }}>
           <Graticule stroke="#F53" />
           <Geographies geography="/features.json">
@@ -62,9 +72,38 @@ const travel = () => {
             }
           </Geographies>
         </ComposableMap>
+        <div className="text-neon-green font-extrabold text-3xl">
+          country notes
+        </div>
+        <br />
+        <ul className="pl-5 text-xl">
+          {allTravelData.map(({ id, date, title }) => (
+            <li className="mb-4" key={id}>
+              <Link
+                className={`hover:text-neon-green`}
+                key={title}
+                href={`/travel/${id}`}
+              >
+                <span className="font-light mr-4">{date}</span>
+                <u className="no-underline">{title}</u>
+              </Link>
+              <br />
+            </li>
+          ))}
+        </ul>
       </Layout>
     </>
   );
 };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allTravelData = getSortedData("travel");
+  return {
+    props: {
+      allTravelData,
+    },
+  };
+};
+
 
 export default travel;
