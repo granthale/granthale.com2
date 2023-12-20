9ecb89442a1f62b6c1cfb7e367a101942d712b94
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-12-20 18:55:31 +0530

    Reorganized friends & people

--- pages/seeds/friends-n-people.tsx
+++ pages/seeds/friends-n-people.tsx
@@ -15,35 +15,8 @@ const friendsNPeople = () => {
 
         <br />
         <section>
-          <h1 className="font-bold">Internet-specific</h1>
-          <ul className="pl-5 list-disc">
-            <li>
-              <a
-                href="https://guzey.com/how-to-make-friends-over-the-internet/#the-most-useful-post-on-networking-ive-ever-read"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                How to Make Friends over the Internet by Alexey Guzey
-                <ExternalArrow />
-              </a>
-            </li>
-          </ul>
-        </section>
-
-        <br />
-        <section>
           <h1 className="font-bold">General Advice</h1>
           <ul className="pl-5 list-disc">
-            <li>
-              <a
-                href="https://benjaminreinhardt.com/networking-for-nerds/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Networking for Nerds by Benjamin Reinhardt
-                <ExternalArrow />
-              </a>
-            </li>
             <li>
               <a
                 href="https://www.lesswrong.com/posts/pfibDHFZ3waBo6pAc/intentionally-making-close-friends"
@@ -56,11 +29,48 @@ const friendsNPeople = () => {
             </li>
             <li>
               <a
+                href="https://benjaminreinhardt.com/networking-for-nerds/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Networking for Nerds by Benjamin Reinhardt
+                <ExternalArrow />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://waitbutwhy.com/2014/12/10-types-odd-friendships-youre-probably-part.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                10 Types of Odd Friendships... by Tim Urban
+                <ExternalArrow />
+              </a>
+            </li>
+            <li>
+              <a
                 href="https://www.swyx.io/friendcatchers"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Friendcatchers by Swyx
+                <ExternalArrow />
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <br />
+        <section>
+          <h1 className="font-bold">Internet-specific</h1>
+          <ul className="pl-5 list-disc">
+            <li>
+              <a
+                href="https://guzey.com/how-to-make-friends-over-the-internet/#the-most-useful-post-on-networking-ive-ever-read"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                How to Make Friends over the Internet by Alexey Guzey
                 <ExternalArrow />
               </a>
             </li>
