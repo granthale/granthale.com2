28974c3556e73beff895fde135b00a8cd4027b64
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-12-21 20:28:35 +0300

    Adjusted color scheme

--- pages/404.tsx
+++ pages/404.tsx
@@ -18,14 +18,14 @@ export default function Custom404() {
       <Header page="404" />
       <Layout>
         <div className={styles.container}>
-          <h1 className="text-4xl text-neon-green">404 - Page not found.</h1>
+          <h1 className="text-4xl text-blue">404 - Page not found.</h1>
           <br />
           <h1>
             {" "}
             <span className="ascii text-3xl">(╯°□°）╯︵ ┻━┻</span>
           </h1>
           <br />
-          <button className="text-3xl hover:text-neon-green" onClick={goBack}>
+          <button className="text-3xl hover:text-blue" onClick={goBack}>
             Go back
           </button>
         </div>
