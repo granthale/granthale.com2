4df2dea0c9ff1a8a2a755b85f4deb9750ff0a701
Grant Hale <64755492+granthale@users.noreply.github.com>
2024-02-16 16:46:37 -0600

    Fixed library grid by including a util in my tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{ts,tsx}", 
-    "./components/**/*.{ts,tsx}"],
-  darkMode: 'media',
-    theme: {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./libraryUtils.tsx",
+  ],
+  darkMode: "media",
+  theme: {
     extend: {
       colors: {
-        "primary": '#2563eb',
-        "neon-green": '#22c55e',
-        "waving-brown": '#F4C641',
-        "white": "#FFFFFF",
-        "background": "#FDF3F1",
-        "gray": "#808080",
-        "reddish":"#E55C41",
-        "black": "#000000",
-        "blue": "#2563eb",
-        "violet": "#7c3aed",
+        primary: "#2563eb",
+        "neon-green": "#22c55e",
+        "waving-brown": "#F4C641",
+        white: "#FFFFFF",
+        background: "#FDF3F1",
+        gray: "#808080",
+        reddish: "#E55C41",
+        black: "#000000",
+        blue: "#2563eb",
+        violet: "#7c3aed",
       },
     },
-    },
+  },
   variants: {
     extend: {
-      typography: ['dark'],
-  }},
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-}
+      typography: ["dark"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
